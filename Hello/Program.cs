741b90fc2b357f7c8e07ea67ac1f788bbaeedce0
Ansh agrawal <agrawalansh624@gmail.com>
2025-08-13 14:30:40 +0530

    getting started with data ttypes in c sharp

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -20,6 +20,23 @@ namespace Hello
 
             Console.Write("This is a single line output ");
 
+
+            // data types in c sharp
+
+            /*
+             * Integer -- > 4 bytes
+             * Long     -- > 8 bytes
+             * FLoating --> 4 bytes
+             * Double --> 8 bytes
+             * Char --> 2 bytes
+             * String  --> 2 bytes per character
+             * Boolean --> 1 bit
+             */
+
+            string input = Console.ReadLine(); // takes input from the user in string format
+
+            Console.WriteLine("You entered: " + input); // outputs the input back to the user
+
         }
     }
 }
